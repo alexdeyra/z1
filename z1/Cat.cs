fc7a51e9f8680115d11fa0548d10775f39f8928c
shugu <shugu@BANBAN>
2024-11-02 11:11:54 +0500

    Добавьте файлы проекта.

---
+++ z1/Cat.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using z1;
+
+namespace z1
+{
+    class Cat
+    {
+        private string name; // скрытое поле для имени кота
+        private double weight; // скрытое поле для веса кота
+
+        public Cat(string catName, double initialWeight)
+        {
+            Name = catName; // Устанавливаем имя кота через свойство Name
+            Weight = initialWeight; // Устанавливаем вес кота через свойство Weight
+        }
+        public string Name // свойство, реализуем инкапсуляцию!
+        {
+            // получение значения - просто возврат name
+            get
+            {
+                return name; // Возвращаем текущее значение имени
+            }
+            // установка значения - используем проверку
+            set
+            {
+                bool OnlyLetters = true;
+                // ключ. слово value - это то, что хотят свойству присвоить
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+
+                if (OnlyLetters)
+                {
+                    name = value;
+                    Console.WriteLine($"{name}: МЯЯЯЯУ!!!!");
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                }
+            }
+        }
+        public double Weight // Свойство для доступа к весу кота
+        {
+            get 
+            { 
+                return weight; 
+            } // Геттер для получения значения веса
+            set
+            {
+                if (value > 0) // Проверка, что вес больше нуля
+                {
+                    weight = value; // Устанавливаем вес кота
+                    Console.WriteLine($"{name} весит: {weight} кг"); // Выводим сообщение с весом
+                }
+                else // Если вес меньше или равен нулю
+                {
+                    Console.WriteLine($"{value} кг: Вес не может быть нулевым или отрицательным"); // Сообщение об ошибке
+                }
+            }
+        }
+    }
+}
+
---
+++ z1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace z1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя кота: "); // Запрос имени кота у пользователя
+            string catName = Console.ReadLine();  // Чтение имени кота из ввода пользователя
+            double initialWeight;  // Объявление переменной для хранения веса кота
+            Console.Write("Введите вес кота (положительное число): "); //Запрос веса кота у пользователя
+            while (!double.TryParse(Console.ReadLine(), out initialWeight)|| initialWeight <=0 ) //Цикл для проверки корректности ввода веса
+            {
+                Console.WriteLine("Ошибка: Пожалуйста, введите положительное число."); // Сообщение об ошибке, если ввод некорректен
+                Console.Write("Введите вес кота (положительное число):"); 
+            }
+
+            Cat myCat = new Cat(catName, initialWeight); // Создание объекта Cat с именем и весом
+            Console.ReadKey(); // Ожидание нажатия клавиши перед завершением программы
+        }
+    }
+
+}
